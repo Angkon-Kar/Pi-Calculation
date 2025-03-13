87f0d2d8a57fcc9bba844e75d3b9a15b9a8792b0
Angkon Kar <angkon310@gmail.com>
2025-03-14 02:49:15 +0600

    Adding Using_M_Pi.cpp

---
+++ Using_M_Pi.cpp
@@ -0,0 +1,16 @@
+/*
+Why Use M_PI?
+It provides a high-precision value of π.
+Avoids hardcoding 3.14159 manually.
+Useful in trigonometric calculations like sin(), cos(), etc.
+*/
+
+#include <iostream>
+#include <cmath> // Required for M_PI
+
+using namespace std;
+
+int main() {
+    cout << "The value of Pi is: " << M_PI << endl;
+    return 0;
+}
